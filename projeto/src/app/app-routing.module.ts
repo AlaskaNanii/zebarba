99687c8ea50645fe1088351a5784ed607da2676b
Dev-hondeiro <kaykerico0203@gmail.com>
2024-10-02 16:05:38 -0300

    tela login teste

--- projeto/src/app/app-routing.module.ts
+++ projeto/src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  
+  
 ];
 @NgModule({
   imports: [
