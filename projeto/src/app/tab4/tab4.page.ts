2d661cdd581066d0bca66153c1954aae6403c721
andrefelipessz <andrefelipe3sds@yahoo.com>
2024-11-13 09:54:58 -0300

    Página de Apresentação

--- projeto/src/app/tab4/tab4.page.ts
+++ projeto/src/app/tab4/tab4.page.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
+  people = [
+    { name: 'André Felipe Silva de Souza', age: 22, role: 'O Mestre', email: 'andrefelipe.silvasouza@souunisuam.com.br', photo: 'assets/imgs/pessoa1.jpg' },
+    { name: 'Kayke Bernardo Gomes', age: 20, role: 'Css man', email: 'kaykegomes@souusisuam.com.br', photo: 'assets/imgs/pessoa2.jpg' },
+    { name: 'Romário Gonzaga', age: '20 (Supostamente)', role: 'Botafoguense', email: 'pessoa3@example.com', photo: 'assets/imgs/pessoa3.jpg' },
+    { name: 'Wallace Pfaltzgraff pessanha', age: 20, role: 'Ginecofego', email: 'wallace.pfa@hotmail.com', photo: 'assets/imgs/pessoa4.jpg' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
+
+
